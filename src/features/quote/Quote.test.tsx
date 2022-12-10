c5c81d75feafb10fa7de9f308ef6f191e234459b
MauriBoye <mauriboyee@gmail.com>
2022-12-09 22:48:13 -0300

    Tests

--- src/features/quote/Quote.test.tsx
+++ src/features/quote/Quote.test.tsx
@@ -20,7 +20,7 @@ describe("QuoteComponent", () => {
   it("should render loading when click the character button", async () => {
     render(<Quote />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "Milhouse");
+    userEvent.type(input, "Milhouse Van Houten");
     await waitFor(() => {
       userEvent.click(screen.getByText("Obtener Cita"));
     });
@@ -30,12 +30,38 @@ describe("QuoteComponent", () => {
   it("should render character quote when click button with a valid input value", async () => {
     render(<Quote />);
     const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    userEvent.type(input, "Milhouse");
+    userEvent.type(input, "Milhouse Van Houten");
     await waitFor(() => {
       userEvent.click(screen.getByText("Obtener Cita"));
     });
     expect(
       await screen.findByText("But my mom says I'm cool.")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("Bart Simpson")
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render random quote when click button without an input value", async () => {
+    render(<Quote />);
+    userEvent.click(screen.getByText("Obtener cita aleatoria"));
+    expect(
+      await screen.findByText("Thank you. Come again.")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("No se encontro ninguna cita")
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render error when not found character name", async () => {
+    render(<Quote />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    userEvent.type(input, "Mauricio Boye");
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Obtener Cita"));
+    });
+    expect(
+      await screen.findByText("Por favor ingrese un nombre válido")
     ).toBeInTheDocument();
   });
 
